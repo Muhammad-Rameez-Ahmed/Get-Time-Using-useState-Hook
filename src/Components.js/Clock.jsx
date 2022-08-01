327b4d8691c142402d3f57b6d1cfb6181f250c01
Rameez <muhammadrameezahmed73@gmail.com>
2022-08-01 11:29:05 +0500

    first commit

---
+++ src/Components.js/Clock.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+const Clock = () => {
+    let nowtime = new Date().toLocaleTimeString();
+    const [time, setTime] = useState(nowtime);
+
+
+    const currentTime = () => {
+        let time = new Date().toLocaleTimeString();
+
+        setTime(time);
+    }
+
+    return (
+
+        <>
+            <h1 className='text'>{time}</h1>
+            <button className='button' onClick={currentTime}>Get Time</button>
+            <h1></h1>
+        </>
+    )
+}
+
+export default Clock
